fd9620b99c64882b282c069110c2979413ad08ba
mrkoppy <jiale04_30@hotmail.com>
2018-08-26 23:25:07 +0800

    - some data once users click into available trip can display to interface already

--- app/src/main/java/com/example/mrkoppy/myappfyp/my_available_trip_insidedata.java
+++ app/src/main/java/com/example/mrkoppy/myappfyp/my_available_trip_insidedata.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class my_available_trip_insidedata extends AppCompatActivity {
+    TextView tv_origin,tv_destination,tv_estimatetime,tv_estimatecost;
+    /*String str_postfrom;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,17 @@ public class my_available_trip_insidedata extends AppCompatActivity {
 
         Post_availabletrip post = (Post_availabletrip) getIntent().getSerializableExtra("post");
         Log.i("sdsddsds", post.From);
+
+        tv_origin = (TextView)findViewById(R.id.tv_fromLocation);
+        tv_origin.setText(post.From);
+        tv_destination = (TextView)findViewById(R.id.tv_tolocation);
+        tv_destination.setText(post.To);
+        tv_estimatetime = (TextView)findViewById(R.id.tv_estimatetime);
+        tv_estimatetime.setText(post.Est);
+        tv_estimatecost = (TextView)findViewById(R.id.tv_estimatecost);
+        tv_estimatecost.setText(post.Price);
+
+
     }
 
     public void btn_book(View view){
